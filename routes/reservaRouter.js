9d7a4f124050305a0ff77f1de8ace385aeeeb0ac
car0l15 <carol.nepomuceno95@gmail.com>
2023-09-28 17:05:39 -0300

    refactor: refatorando alguns pontos de sintaxe

--- routes/reservaRouter.js
+++ routes/reservaRouter.js
@@ -5,8 +5,8 @@ const router = express.Router()
 const reservaController = require('../controllers/reservaController')
 
 
-router.post('/reservas', reservaController.postReserva)
-router.get('/reservas', reservaController.getReservaPorPlaca)
+router.post('/reserva', reservaController.postReserva)
+router.get('/reserva', reservaController.getReservaPorPlaca)
 router.put('/reserva', reservaController.putReserva)
 
 module.exports = router
